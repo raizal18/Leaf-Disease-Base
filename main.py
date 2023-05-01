06a8fa69478a81a964913b8e5760dcabdc5cde7d
raizal18 <ramakrishnanmurugan6@gmail.com>
2023-05-01 20:18:15 +0530

    data info

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ import os
 import sys
 import matplotlib.pyplot as plt
 
-DATA_PATH = ""
+
+# // Set path of image dataset //
+ 
+DATA_PATH = "D:\PHD\Leaf Disease\CODE\plantvillage dataset"
+
+for root , direct, files in os.walk(DATA_PATH):
+    print(root)
+    print(direct)
