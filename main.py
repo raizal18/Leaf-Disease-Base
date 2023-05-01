3023717fb49a1743cb94d4702da39a11c914667a
raizal18 <ramakrishnanmurugan6@gmail.com>
2023-05-01 20:43:27 +0530

    changes

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import os
 import sys
+import cv2
+import logging
 import matplotlib.pyplot as plt
 
 
 # // Set path of image dataset //
  
-DATA_PATH = "D:\PHD\Leaf Disease\CODE\plantvillage dataset"
+DATA_PATH = "D:/PHD/Leaf Disease/CODE/plantvillage dataset"
 
-for root , direct, files in os.walk(DATA_PATH):
-    print(root)
-    print(direct)
+_COLOR_IMAGE = os.path.join(DATA_PATH,'color')
+
+total_counts = 0
+
+for index, (root , direct, files) in enumerate(os.walk(_COLOR_IMAGE)):
+    print(f"{index} {root} counts {len(files)}")
+    total_counts += len(files)
+
