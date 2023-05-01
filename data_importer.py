26fc1a066f597b0210634f539078abeabc9e9652
raizal18 <ramakrishnanmurugan6@gmail.com>
2023-05-01 22:22:27 +0530

    updated

---
+++ data_importer.py
@@ -0,0 +1,23 @@
+import os
+import numpy
+import sys
+from keras.preprocessing.image import ImageDataGenerator
+
+def get_images(filepath:set) -> object:
+
+    data_gen = ImageDataGenerator()
+    image_generator = data_gen.flow_from_directory(filepath)
+    return image_generator
+
+def get_segment_mask(filepath):
+
+    pass
+
+
+def get_train_data(filepath):
+    pass
+
+
+def get_test_data(filepath):
+    pass
+    
